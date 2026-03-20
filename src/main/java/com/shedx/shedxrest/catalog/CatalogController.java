2bed884aa906843331ea86bb934d9380b0b7274b
ShedXBr <shedx99@gmail.com>
2026-03-20 17:44:29 -0300

    Only Admin can register catalog

--- src/main/java/com/shedx/shedxrest/catalog/CatalogController.java
+++ src/main/java/com/shedx/shedxrest/catalog/CatalogController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shedx.shedxrest.auth.UserEntity;
+import com.shedx.shedxrest.auth.UserRepository;
 import com.shedx.shedxrest.auth.company.CompanyEntity;
 import com.shedx.shedxrest.auth.company.companyRepository;
 import com.shedx.shedxrest.security.TenantContext;
@@ -23,10 +26,11 @@ import com.shedx.shedxrest.security.TenantContext;
 public class CatalogController {
     private final CatalogRepository repo;
     private final companyRepository Crepo;
-    
-    public CatalogController(CatalogRepository repo, companyRepository Crepo){
+    private final UserRepository Urepo;
+    public CatalogController(CatalogRepository repo, companyRepository Crepo, UserRepository Urepo){
         this.repo = repo;
         this.Crepo = Crepo;
+        this.Urepo = Urepo;
         
     }
     @GetMapping
@@ -36,16 +40,29 @@ public class CatalogController {
         return repo.findByCompanyId(company.getId());
     }
     @PostMapping("/register")
-    public ResponseEntity<String> create(@RequestBody Catalog catalog){
+    public ResponseEntity<String> create(@RequestBody Catalog catalog, Authentication auth){
         CompanyEntity company = Crepo.findById(TenantContext.getCompanyId())
         .orElseThrow(()->new BadCredentialsException("Invalid Company"));
-        Catalog catalogEn = new Catalog();
-        catalogEn.setName(catalog.getName());
-        catalogEn.setPrice(catalog.getPrice());
-        catalogEn.setDescProduct(catalog.getDescProduct());
-        catalogEn.setCompanyId(company.getId());
-        repo.save(catalogEn);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Product registered");
+        UserEntity user;
+        try {
+            user = Urepo.findById(Long.valueOf(auth.getName()))
+            .orElseThrow(()-> new BadCredentialsException("Invalid Auth"));
+        } catch (NumberFormatException e) {
+            throw new BadCredentialsException(e.getMessage());
+        }
+        if(user.getRole().equalsIgnoreCase("ROLE_ADMIN")){
+            Catalog catalogEn = new Catalog();
+            catalogEn.setName(catalog.getName());
+            catalogEn.setPrice(catalog.getPrice());
+            catalogEn.setDescProduct(catalog.getDescProduct());
+            catalogEn.setCompanyId(company.getId());
+            repo.save(catalogEn);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Product registered");
+        }else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User Not Authorized");
+        }
+        
+        
     }
     
 }//Rest to create and list catalogs
